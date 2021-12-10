fbc1cb833c0e8f760f54923c2a949088447db373
Dave Madden <davelearnsstuff@gmail.com>
2021-12-09 23:11:42 -0800

    migration complete. seed underway.

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,10 @@
-// build your `/api/projects` router here
+const express = require('express')
+const router = express.Router()
+
+const Project = require('./model')
+
+
+
+
+
+module.exports = router
